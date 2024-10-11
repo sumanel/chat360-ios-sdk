2321d19a67d4821e2d7685594a07d42aea727840
Harshit Sharma <harshit.sharma@chat360.io>
2024-10-11 12:45:58 +0530

    feat: support for swiftUI, and write test for better url parsing.

---
+++ Sources/chat360_iosSdk/Model/Chat360Config.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Harshit Sharma on 11/10/24.
+//
+
+import Foundation
--- Sources/chat360_iosSdk/chat360_iosSdk.swift
+++
@@ -1,6 +0,0 @@
-public struct chat360_iosSdk {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
